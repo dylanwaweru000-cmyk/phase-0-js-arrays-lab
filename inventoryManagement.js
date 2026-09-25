724294981e087043777e9b26d17faeeea86b46fc
dylanwaweru000-cmyk <dylanwaweru.000@gmail.com>
2026-09-25 10:11:47 +0300

    Complete inventory management lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -10,3 +10,17 @@ module.exports = {
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
   products
 };
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+function addProduct(productName) {
+  products.push(productName);
+}
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+function removeLastProduct() {
+  products.pop();
+}
